c003983ec964e8e3adf41c22652a61b171974547
vKochanov78 <vale78.1998@gmail.com>
2023-10-26 15:42:08 +0300

    Uploading JS homework

---
+++ 01. JS Syntax Fundamentals/06_largest-number.js
@@ -0,0 +1,9 @@
+function largest_number (num1, num2, num3) {
+    const largestNum = Math.max(num1, num2, num3);
+
+    console.log(`The largest number is: ${largestNum}`)
+}
+
+largest_number(54,6,3) // Expected output: The largest number is: 54
+largest_number(5,6,3) // Expected output: The largest number is: 6
+largest_number(5,6,36) // Expected output: The largest number is: 36
