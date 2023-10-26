3a765dec8fa1ed50b0272f239f6828d7843aaa13
vKochanov78 <vale78.1998@gmail.com>
2023-10-26 15:31:37 +0300

    Uploading the JS homework

---
+++ 01. JS Syntax Fundamentals/01_multiply-the-number-by-2.js
@@ -0,0 +1,8 @@
+function multiply_number (num) {
+    console.log(num * 2);
+}
+
+multiply_number(2) // Expected output: 4
+multiply_number(4) // Expected output: 8
+multiply_number(6) // Expected output: 12
+multiply_number(8) // Expected output: 16
