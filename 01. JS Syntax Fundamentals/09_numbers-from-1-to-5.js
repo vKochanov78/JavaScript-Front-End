b126589e961040e1f525a17c10bbb85afb1f24d3
vKochanov78 <vale78.1998@gmail.com>
2023-10-27 09:32:46 +0300

    Uploading JS homework

---
+++ 01. JS Syntax Fundamentals/09_numbers-from-1-to-5.js
@@ -0,0 +1,11 @@
+function number_loop() {
+    for (let i=1; i<=5; i++){
+        console.log(i)
+    }
+}
+
+number_loop()    // Expected output: 1
+//                                   2
+//                                   3
+//                                   4
+//                                   5
