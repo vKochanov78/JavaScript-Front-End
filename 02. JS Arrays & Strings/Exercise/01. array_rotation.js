e987173f4057d2df3ab0f4b3a64b1960b36444c2
vKochanov78 <vale78.1998@gmail.com>
2023-11-06 20:38:59 +0200

    Uploading JS homework

---
+++ 02. JS Arrays & Strings/Exercise/01. array_rotation.js
@@ -0,0 +1,12 @@
+"use strict";
+function arrayRotation(arr, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        const firstElement = arr.shift();
+        arr.push(firstElement);
+    }
+    console.log(arr.join(" "));
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2);
+arrayRotation([32, 21, 61, 1], 4);
+arrayRotation([2, 4, 15, 31], 5);
