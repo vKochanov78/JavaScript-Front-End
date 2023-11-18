c4ff52c0306a292d3659927b4fee267a0a626401
vKochanov78 <vale78.1998@gmail.com>
2023-11-18 10:24:59 +0200

    Uploading JS homework

---
+++ 04. Objects and Classes/Lab/01. Person Info.js
@@ -0,0 +1,8 @@
+function getPersonInfo (firstName, secondName, age) {
+    let person = {firstName, secondName, age}
+
+    return person;
+}
+
+console.log(getPersonInfo("Peter","Pan","20"))
+// console.log(getPersonInfo("George","Smith","18"))
